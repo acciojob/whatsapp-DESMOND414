4799a8406d00615ecd3b84bf4bc8257f0edb2aa7
Algani Uthej <69247162+DESMOND414@users.noreply.github.com>
2023-05-20 17:43:00 +0530

    changes4

--- src/main/java/com/driver/Message.java
+++ src/main/java/com/driver/Message.java
@@ -6,13 +6,16 @@ import java.util.Date;
 public class Message {
     private int id;
     private String content;
+    private Date timestamp;
+
+    public Message(int id, String content, Date timestamp) {
+        this.id = id;
+        this.content = content;
+        this.timestamp = timestamp;
+    }
 
     public Message() {
     }
-
-
-
-    private Date timestamp;
 
     public int getId() {
         return id;
